ec1ea68a7c97f79e66606913226b8b72150ad5bf
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-30 21:30:14 +0200

    Lend Page updates

--- app/lend/page.tsx
+++ app/lend/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "../components/Lend/Hero";
 import React from "react";
 import HowItWorks from "../components/Lend/HowItWorks";
-import AddableItemGrid from "../components/Lend/ListingMockup";
 
 export default function HomePage() {
   return (
