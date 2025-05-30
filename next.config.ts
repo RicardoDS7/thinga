086405c1ecb70700ab089c693e35a977374b94a0
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-30 12:04:15 +0200

    Add workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/your-repo-name",
+  trailingSlash: true, // Optional but recommended for GitHub Pages
 };
 
 export default nextConfig;
